d8f41782910dbfd496cef6dfaa5b8ef8bb8f9789
NisarLDev <harusbadah@proton.me>
2022-10-18 16:38:16 +0200

    Function Strncmp completed

--- Cursus/LibFT/ft_strncmp.c
+++ Cursus/LibFT/ft_strncmp.c
@@ -1,7 +1,14 @@
-//
-// Created by Nisar Laftissi Blasco on 9/24/22.
-//
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 22:59:11 by nlaftiss          #+#    #+#             */
+/*   Updated: 2022/10/18 15:34:36 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
