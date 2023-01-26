44337967169ac3372664b114742198da79cee9e6
NisarLDev <HarusBadah@proton.me>
2023-01-26 07:26:03 +0100

    Added Ft_isflag function

---
+++ Cursus/Ft_Printf/ft_flags_utils.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 15:57:52 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/26 07:22:58 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_isflag(int c)
+{
+	return (ft_istype(c) || ft_isdigit(c) || ft_isspec(c));
+}
+
+int	ft_isspec(int c)
+{
+	if (c == '-' || c == '0' || c == '.' || c == '*'
+		|| c == '#' || c == ' ' || c == '+')
+		return (1);
+	return (0);
+}
+
+int	ft_istype(int c)
+{
+	if (c == 'c' || c == 's' || c == 'd' || c == 'i' || c == 'u'
+		|| c == 'x' || c == 'X' || c == 'p' || c == '%')
+		return (1);
+	return (0);
+}
