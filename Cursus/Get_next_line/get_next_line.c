6dbe576cd4958f7aa742cfa6b094b46cdb4640bf
NisarLDev <harusbadah@proton.me>
2023-02-01 14:31:09 +0100

    Update Get_next_line function: Added basic structure in the get_next_line function

--- Cursus/Get_next_line/get_next_line.c
+++ Cursus/Get_next_line/get_next_line.c
@@ -6,14 +6,19 @@
 /*   By: nlaftiss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 14:14:50 by nlaftiss          #+#    #+#             */
-/*   Updated: 2023/02/01 14:23:30 by nlaftiss         ###   ########.fr       */
+/*   Updated: 2023/02/01 14:30:17 by nlaftiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include
 
 char *get_next_line(int fd);
 {
+	static	t_list	*stash = NULL;
+	char			*line;
+	int				readed;
 
+	if (fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
 }
 
 void	read_and_stash()
