e1164620bbba73fdda6ed796a294b22e142a1918
NisarLDev <harusbadah@proton.me>
2023-02-01 15:01:04 +0100

    Update Get_next_line function: Added basic structure to get_next_line.h

--- Cursus/Get_next_line/get_next_line.h
+++ Cursus/Get_next_line/get_next_line.h
@@ -5,4 +5,19 @@
 # include <stdlib.h>
 
 # include <sys/types.h>
+# include <sys/uio.h>
 # include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#	define BUFFER_SIZE 5
+# endif
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}				t_list;
+
+char	*get_next_line(int fd);
+int		found_newline(t_list *stash)
+t_list	*ft_lst_get_last(t_list *stash);
