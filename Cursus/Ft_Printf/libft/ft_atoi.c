75ad6253f0e5414b2f59302b9668d6d1cb875d2f
NisarLDev <HarusBadah@proton.me>
2023-02-06 14:15:15 +0100

    Added ft_atoi.c

---
+++ Cursus/Ft_Printf/libft/ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 12:15:31 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/04 14:23:45 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_atoi converts a string into an int.
+
+	RETURN VALUE :
+	The converted int.
+*/
+
+int	ft_atoi(const char *str)
+{
+	int	num;
+	int	isneg;
+	int	i;
+
+	num = 0;
+	isneg = 1;
+	i = 0;
+	while (str[i] && (str[i] == ' ' || str[i] == '\t'
+			|| str[i] == '\n' || str[i] == '\r'
+			|| str[i] == '\v' || str[i] == '\f'))
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		isneg *= -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		num = (num * 10) + (str[i] - '0');
+		i++;
+	}
+	return (num * isneg);
+}
