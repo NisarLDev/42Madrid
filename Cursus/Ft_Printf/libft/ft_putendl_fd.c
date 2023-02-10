b2a3aa82d64ac934051b81997c7c1ae64cee0238
NisarLDev <HarusBadah@proton.me>
2023-02-10 23:15:22 +0100

    Added ft_putendl_fd.c function

---
+++ Cursus/Ft_Printf/libft/ft_putendl_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 10:02:34 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/16 13:43:36 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_putendl_fd writes the given string to the given
+	file descriptor followed by a new line.
+
+	RETURN VALUE :
+	None.
+*/
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
