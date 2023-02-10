6fbbc39a698ca8344fc117e59c16dada1acb1f98
NisarLDev <HarusBadah@proton.me>
2023-02-10 15:33:41 +0100

    Added ft_itoa.c function

---
+++ Cursus/Ft_Printf/libft/ft_itoa.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/27 15:12:24 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/04 11:14:25 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_itoa converts the integer n into a string of characters.
+
+	RESULT VALUE :
+	The string of the converted integer.
+*/
+
+static size_t	ft_itoa_len(long num)
+{
+	size_t	len;
+
+	len = 0;
+	if (num == 0)
+		return (1);
+	if (num < 0)
+	{
+		len++;
+		num = -num;
+	}
+	while (num >= 1)
+	{
+		len++;
+		num /= 10;
+	}
+	return (len);
+}
+
+static char	*ft_num_to_str(long num, char *str, size_t len)
+{
+	str = ft_calloc(len + 1, sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	if (num < 0)
+	{
+		str[0] = '-';
+		num = -num;
+	}
+	len--;
+	while (len)
+	{
+		str[len] = (num % 10) + '0';
+		num /= 10;
+		len--;
+	}
+	if (str[0] != '-')
+		str[0] = (num % 10) + '0';
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	long	num;
+	size_t	len;
+	char	*str;
+
+	num = n;
+	len = ft_itoa_len(num);
+	str = 0;
+	str = ft_num_to_str(num, str, len);
+	if (!str)
+		return (NULL);
+	return (str);
+}
