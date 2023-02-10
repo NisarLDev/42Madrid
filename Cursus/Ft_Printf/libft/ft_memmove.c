92303ea77e29721b3500da1b5c67d961a296f2fa
NisarLDev <HarusBadah@proton.me>
2023-02-10 23:00:58 +0100

    Added ft_memmove.c function

---
+++ Cursus/Ft_Printf/libft/ft_memmove.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 10:57:19 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/01 12:31:42 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_memmove copies n bytes from memory area src to memory
+	area dst. The memory areas may overlap: if the dst pointer is found
+	to be between the src pointer and the index n, copying will be done
+	back to front to prevent data being modified before being copied.
+	Otherwise it will be done front to back	to preserve data.
+
+	RETURN VALUE :
+	A pointer to dst.
+*/
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char		*dp;
+	const char	*sp;
+
+	if (src == dst)
+		return (dst);
+	dp = (char *)dst;
+	sp = (const char *)src;
+	if (sp < dp && sp + len > dp)
+		while (len--)
+			*(dp + len) = *(sp + len);
+	else
+	{
+		while (len--)
+		{
+			*dp = *sp;
+			sp++;
+			dp++;
+		}
+	}
+	return (dst);
+}
