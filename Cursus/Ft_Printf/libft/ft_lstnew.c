a8024d627ecf209bd9792e7eaa349b06363cc602
NisarLDev <HarusBadah@proton.me>
2023-02-10 16:27:12 +0100

    Added ft_lstnew.c function

---
+++ Cursus/Ft_Printf/libft/ft_lstnew.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/14 18:28:58 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/20 09:45:16 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_lstnew allocates memory for a new list node and 
+	initializes its content to the value passed as parameter, before
+	setting its next node to NULL. 
+
+	RESULT VALUE :
+	The new list ode.
+*/
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
