753f6cab0a70be8b96355ecab3037bc72b78c867
NisarLDev <HarusBadah@proton.me>
2023-02-10 23:22:48 +0100

    Added ft_putstr_fd.c function

---
+++ Cursus/Ft_Printf/libft/ft_putstr_fd.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/29 11:32:28 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/03 13:53:40 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_putstr_fd writes the given string to the given
+	file descriptor.
+
+	RETURN VALUE :
+	None.
+*/
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	while (*s != '\0')
+	{
+		ft_putchar_fd(*s, fd);
+		s++;
+	}
+}
