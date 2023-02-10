66245892b0bf7454879be47618a41ce98f77dbc6
NisarLDev <HarusBadah@proton.me>
2023-02-10 23:42:08 +0100

    Added ft_striteri.c function

---
+++ Cursus/Ft_Printf/libft/ft_striteri.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 12:10:52 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/29 10:04:25 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_striteri applies the given function f to each
+	character in the given string s.
+
+	RETURN VALUE :
+	None.
+*/
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		(*f)(i, &s[i]);
+		i++;
+	}
+}
