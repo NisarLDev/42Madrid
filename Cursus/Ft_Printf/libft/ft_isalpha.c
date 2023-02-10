6092a846dfdcde11f5d0fc54fa0e0d5494df17ec
NisarLDev <HarusBadah@proton.me>
2023-02-10 15:17:11 +0100

    Added ft_isalpha.c function

---
+++ Cursus/Ft_Printf/libft/ft_isalpha.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 12:20:31 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/01 18:42:57 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	DESCRIPTION :
+	The function ft_isalpha checks whether c is alphabetic or not.
+
+	RETURN VALUE :
+	Non-zero if c is alphabetic, zero if not.
+*/
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (c);
+	return (0);
+}
