b2666d7b1198731f317f37a7f9b9333efb62bbf9
NisarLDev <HarusBadah@proton.me>
2023-02-10 16:22:27 +0100

    Added ft_lstmap.c function

---
+++ Cursus/Ft_Printf/libft/ft_lstmap.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/14 20:36:31 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/19 11:36:21 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_lstmap creates a new list from a given list  by 
+	applying the function passed as parameter to the original list. If
+	the memory allocation fails for any node in the new list, the new list
+	will be deleted with the function passed as parameter and its memory
+	will be freed.
+
+	RETURN VALUE :
+	The new list containing the new values if a functon was provided.
+	A new copy of the list if no function was provided.
+	NULL if the memory allocation failed.
+*/
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*newlst;
+	t_list	*node;
+
+	if (!lst)
+		return (NULL);
+	newlst = NULL;
+	node = NULL;
+	while (lst)
+	{
+		if (!f)
+			node = ft_lstnew(lst->content);
+		else
+			node = ft_lstnew(f(lst->content));
+		if (!node)
+		{
+			ft_lstclear(&newlst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlst, node);
+		lst = lst->next;
+	}
+	return (newlst);
+}
