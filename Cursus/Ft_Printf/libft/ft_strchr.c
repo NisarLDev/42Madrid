d91a83927af2c53344c41feaf998dd3742cc61f9
NisarLDev <HarusBadah@proton.me>
2023-02-10 23:34:02 +0100

    Added ft_strchr.c function

---
+++ Cursus/Ft_Printf/libft/ft_strchr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/21 11:03:33 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/05 14:51:48 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_strchr finds the first occurence of character c in
+	string str.
+
+	RETURN VALUE :
+	A pointer to the first occurence of c in str.
+	NULL if c is not found.
+*/
+
+char	*ft_strchr(const char *str, int c)
+{
+	int				i;
+	unsigned char	ch;
+
+	i = 0;
+	ch = c;
+	if (ch == '\0')
+	{
+		i = ft_strlen(str);
+		return ((char *)str + i++);
+	}
+	while (str[i])
+	{
+		if (str[i] == ch)
+			return ((char *)str + i);
+		i++;
+	}
+	return (NULL);
+}
