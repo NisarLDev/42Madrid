d09af0a176c5f2adbe9d48e06244506aa4cc08fb
NisarLDev <HarusBadah@proton.me>
2023-02-10 16:40:51 +0100

    Added ft_memchr.c function

---
+++ Cursus/Ft_Printf/libft/ft_memchr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 20:37:17 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/24 17:31:15 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_memchr searches n bytes of the memory area pointed to
+	by s for the first occurence of c. Both c and the bytes of s are
+	interpreted as unsigned char.
+
+	RETURN VALUE:
+	A pointer to the matching byte. NULL if the character does not occur
+	in the given memory area.
+*/
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t				i;
+	unsigned char		ch;
+	const unsigned char	*str;
+
+	ch = c;
+	str = (const unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == ch)
+			return ((void *)s + i);
+		i++;
+	}
+	return (0);
+}
