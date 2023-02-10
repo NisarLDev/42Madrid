64fbf3e980e5355bf8585597f4adf18e3d4cd488
NisarLDev <HarusBadah@proton.me>
2023-02-10 16:13:21 +0100

    Added ft_lstlast.c function

---
+++ Cursus/Ft_Printf/libft/ft_lstlast.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/14 19:39:52 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/14 20:52:21 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_lstlast finds the last node in a given list.
+
+	RETURN VALUE :
+	The last node of a list.
+		[.]->[.]->[.]->[LAST]->[NULL]
+*/
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst != NULL && lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
