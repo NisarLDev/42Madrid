177c1ca13aab37296767eb782846995ee97fe935
NisarLDev <HarusBadah@proton.me>
2023-02-10 23:47:31 +0100

    Added ft_strlcat.c function

---
+++ Cursus/Ft_Printf/libft/ft_strlcat.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 11:41:17 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/02 09:37:21 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_strlcat appends the given string src to the end of 
+	dst. It will append at most dstsize - ft_strlen(dst) - 1 and 
+	nul-terminate the result.
+
+	Note : space for the terminating \0 character must be included in dstsize.
+
+	RETURN VALUE :
+	The total length of the string that it tried to create : the initial
+	length of dst + the length of src, with the goal to facilitate
+	truncaction detection.
+*/
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	d_size;
+	size_t	s_size;
+
+	d_size = ft_strlen(dst);
+	s_size = ft_strlen(src);
+	if (dstsize <= d_size)
+		return (dstsize + s_size);
+	i = d_size;
+	j = 0;
+	while ((i + j) < (dstsize - 1) && src[j] != '\0')
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (d_size + s_size);
+}
