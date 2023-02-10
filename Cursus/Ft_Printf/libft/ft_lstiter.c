feb384e8fe13c45fb0a7f3300edff15c9ae71be3
NisarLDev <HarusBadah@proton.me>
2023-02-10 15:50:58 +0100

    Added ft_lstiter.c function

---
+++ Cursus/Ft_Printf/libft/ft_lstiter.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/14 20:28:24 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/14 20:50:41 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_lstiter applies the function f passed as parameter
+	to the content of each node of a given list.
+
+	RETURN VALUE :
+	None.
+*/
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!f || !lst)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
