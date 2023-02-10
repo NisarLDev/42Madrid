d6597347d0cf0aaf270cce156bc7738eb076de53
NisarLDev <HarusBadah@proton.me>
2023-02-11 00:02:43 +0100

    Added ft_strlen.c function

---
+++ Cursus/Ft_Printf/libft/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/21 11:21:11 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/01 10:37:56 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_strlen measures the length of the given string str,
+	excluding the terminating \0 character.
+
+	RETURN VALUE :
+	The number of bytes in the string str.
+*/
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
