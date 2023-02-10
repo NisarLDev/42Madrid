099bfa853aa6d1a6504edd5a2b7863b94c9f2517
NisarLDev <HarusBadah@proton.me>
2023-02-10 22:51:48 +0100

    Added ft_memcmp.c function

---
+++ Cursus/Ft_Printf/libft/ft_memcmp.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 10:43:16 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/02 12:53:12 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_memcmp compares the first n bytes of the memory areas
+	s1 and s2. The bytes are interpreted as unsigned char.
+
+	RETURN VALUE :
+	An integer less than, equal to, or greater than zero if the first
+	n bytes of s1 is found to be less than, equal to, or greater than the 
+	first n bytes of s2. Zero if n is equal to zero.
+*/
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const char	*str1;
+	const char	*str2;
+	size_t		i;
+
+	if (n == 0)
+		return (0);
+	str1 = (const char *)s1;
+	str2 = (const char *)s2;
+	i = 0;
+	while ((i < n - 1) && str1[i] == str2[i])
+		i++;
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+}
