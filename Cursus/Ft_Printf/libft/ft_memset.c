8f7426ee5ccb1c15022d8d8ba5c756eab1865873
NisarLDev <HarusBadah@proton.me>
2023-02-10 23:06:12 +0100

    Added ft_memset.c function

---
+++ Cursus/Ft_Printf/libft/ft_memset.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 09:32:16 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/06 13:18:22 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_memset fills the first len bytes of the memory area
+	pointed	to by b with the byte c. Both b and c are interpreted as 
+	unsigned char.
+
+	RETURN VALUE :
+	A pointer to memory area s.
+*/
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*p;
+	unsigned char	ch;
+
+	p = (unsigned char *)b;
+	ch = c;
+	while (len--)
+	{
+		*p = ch;
+		p++;
+	}
+	return (b);
+}
