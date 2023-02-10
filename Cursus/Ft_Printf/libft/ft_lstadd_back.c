871cc74fb0319f3836f29bf8fe8dddf8743d81d8
NisarLDev <HarusBadah@proton.me>
2023-02-10 15:37:46 +0100

    Added ft_lstadd_back.c function

---
+++ Cursus/Ft_Printf/libft/ft_lstadd_back.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/14 18:43:52 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/19 10:31:45 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_lstadd_back adds a new node to the back of a list:
+		[.]->[.]->[.]->[NEW]->[NULL]
+
+	RETURN VALUE :
+	None.
+*/
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list	*tmp;
+
+	if (!new)
+		return ;
+	if (!*alst)
+	{
+		*alst = new;
+		return ;
+	}
+	tmp = ft_lstlast(*alst);
+	tmp->next = new;
+}
