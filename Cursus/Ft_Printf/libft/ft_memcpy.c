fd40e35e84e997c7d71214bfda4eba2e0eb532e5
NisarLDev <HarusBadah@proton.me>
2023-02-10 22:57:38 +0100

    Added ft_memcpy.c function

---
+++ Cursus/Ft_Printf/libft/ft_memcpy.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/21 12:05:36 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/02/05 11:31:40 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_memcpy copies n bytes from memory area src to memory
+	area dst.
+	Does not account for memory overlaps. Use ft_memmove if the memory areas
+	overlap or might overlap.
+
+	RETURN VALUE :
+	A pointer to dst. NULL if src and dst are both NULL.
+*/
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*dp;
+	const char	*sp;
+
+	if (!dst && !src)
+		return (0);
+	if (n == 0 || (dst == src))
+		return (dst);
+	dp = (char *)dst;
+	sp = (const char *)src;
+	while (n != 0)
+	{
+		if (*dp != *sp)
+			*dp = *sp;
+		dp++;
+		sp++;
+		n--;
+	}
+	return (dst);
+}
