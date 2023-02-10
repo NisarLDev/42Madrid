8ef896810eb1e35d3259f3bb8955e9c5d8eb5f48
NisarLDev <HarusBadah@proton.me>
2023-02-10 15:13:46 +0100

    Added ft_isalnum.c function

---
+++ Cursus/Ft_Printf/libft/ft_isalnum.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaftiss <nlaftiss@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/13 10:51:40 by nlaftiss          #+#    #+#             */
+/*   Updated: 2023/01/13 18:58:31 by nlaftiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_isalnum checks whether the value of c is alphanumeric.
+
+	RETURN VALUE :
+	Non-zero if c is alphanumeric, zero if not.
+*/
+
+int	ft_isalnum(int c)
+{
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (c);
+	return (0);
+}
